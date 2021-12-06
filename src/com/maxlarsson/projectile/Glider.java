3def2500f8d1ccfd8e8dbdc41d66d9a2e26bb7b6
Max Larsson <maxslarsson@gmail.com>
2021-12-06 14:40:49 -0500

    Added initializer for Glider

--- src/com/maxlarsson/projectile/Glider.java
+++ src/com/maxlarsson/projectile/Glider.java
@@ -1,2 +1,13 @@
-package com.maxlarsson.projectile;public class Glider {
-}
+package com.maxlarsson.projectile;
+
+//public class Glider extends Particle {
+//    Glider(double x, double y, double vX, double vY, double mass, double radius, double volume,) {
+//        super(x, y);
+//        this.vX = vX;
+//        this.vY = vY;
+//        this.mass = mass;
+//        this.radius = radius;
+//        this.pixRadius = radius;
+//
+//    }
+//}
